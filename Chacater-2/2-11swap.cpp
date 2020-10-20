c14c3a7bbf6cbbcc5ea157db013e268f8a750581
zoomdong <1344492820@qq.com>
2020-10-20 16:17:25 +0800

    update

--- Chacater-2/2-11swap.cpp
+++ Chacater-2/2-11swap.cpp
@@ -10,14 +10,14 @@ void inplace_swap(int *x, int *y) {
 void reverse_array(int a[], int cnt) {
   int first, last;
   // 长度为奇数的时候中间元素回成为0
-  for (first = 0, last = cnt - 1; first <= last; first++, last--)
+  for (first = 0, last = cnt - 1; first < last; first++, last--)
   {
     inplace_swap(&a[first], &a[last]);
   }
 }
 
 int main() {
-  int a[5] = {1, 2, 3, 4, 5};
+  int a[5] = {1, 2, 3, 2, 5};
   reverse_array(a, 5);
   for (int i = 0; i < 5; i++)
   {
