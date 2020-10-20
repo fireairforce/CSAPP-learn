1ce1e73ee7ca07705fefe252dd2fcc749c2e5f67
zoomdong <1344492820@qq.com>
2020-10-20 16:07:47 +0800

    feat: add character-2

---
+++ Chacater-2/print_bytes.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+  size_t i;
+  for (i = 0;i<len;i++) {
+    printf(" %.2x", start[i]);
+  }
+  printf("\n");
+}
+
+void show_init (int x) {
+  show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float (float x) {
+  show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+  show_bytes((byte_pointer) &x, sizeof(void *));
+}
